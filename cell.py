8c29d53ddc6a54b6e3b90a959ead927c463d2a2e
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-12-25 22:19:52 +0530

    multiple types tested

--- cell.py
+++ cell.py
@@ -1,6 +1,8 @@
 
 class Cell:
+    cell_id = 0
     def __init__(self, type, connections):
+        self.cell_id=self.cell_id+1
         self.type = type
         self.connections = connections
 
--- main.py
+++ main.py
@@ -4,5 +4,14 @@ import neurontype as nt
 neuronType = nt.NeuronType("Interneurons", "Multipolar", "Excitatory", "Myelinated", "Mirror")
 
 cell1 = c.Cell(neuronType, [])
+cell2 = c.Cell(neuronType, [])
+cell3 = c.Cell(neuronType, [])
+cell4 = c.Cell(neuronType, [])
+cell5 = c.Cell(neuronType, [])
 
 print(cell1.details().type.getType().function)
+print(cell1.cell_id)
+print(cell2.cell_id)
+print(cell3.cell_id)
+print(cell4.cell_id)
+print(cell5.cell_id)
