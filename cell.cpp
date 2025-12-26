7ddb649a9920bb132bd05069ffc5fbae555f9c13
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-12-26 18:55:01 +0530

    updated

--- cell.cpp
+++
@@ -1,9 +0,0 @@
-class Cell() {
-    public:
-        Cell(int x, int y) : x_(x), y_(y) {}
-        int getX() const { return x_; }
-        int getY() const { return y_; }
-    private:
-        int x_;
-        int y_;
-};
