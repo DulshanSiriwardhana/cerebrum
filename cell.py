66382fc0d2e3077f62db093612478d1fe5e530cf
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-12-26 16:15:14 +0530

    updated

--- cell.py
+++ cell.py
@@ -11,9 +11,15 @@ class Cell:
     def insertConnection(self, connection):
         self.connections.append(connection)
 
-    def fire(self):
+    def fire(self, input):
         #I am thinking bruh
-        print("I am thinking bruh")
+        print("I am thinking bruh: ", input, {
+            "id": self.cell_id,
+            "type": self.type.getType(),
+            "connections": self.connections 
+        })
+        for contact in self.connections:
+            contact.fire(1)
     
     def details(self):
         return {
--- main.py
+++ main.py
@@ -4,13 +4,9 @@ import neurontype as nt
 neuronType = nt.NeuronType("Interneurons", "Multipolar", "Excitatory", "Myelinated", "Mirror")
 
 cell1 = c.Cell(neuronType, [])
-cell2 = c.Cell(neuronType, [])
-cell3 = c.Cell(neuronType, [])
-cell4 = c.Cell(neuronType, [])
-cell5 = c.Cell(neuronType, [])
+cell2 = c.Cell(neuronType, [cell1])
+cell3 = c.Cell(neuronType, [cell1, cell2])
+cell4 = c.Cell(neuronType, [cell2])
+cell5 = c.Cell(neuronType, [cell1, cell2, cell3, cell4])
 
-print(cell1.details())
-print(cell2.details())
-print(cell3.details())
-print(cell4.details())
-print(cell5.details())
+cell5.fire(1)
